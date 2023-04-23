7a0074aedcb2d60f511a83bc630d317a46cfd67b
ThaiBao011002 <123188501+ThaiBao011002@users.noreply.github.com>
2023-04-23 15:56:13 +0700

    code

---
+++ shoeshop/src/modules/PhoneShop/ProductItem.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class ProductItem extends Component {
+	render() {
+		const { productsData, handleSeeDetails, } = this.props;
+		return (
+			<div className='card'>
+				<img src={productsData.image} alt='' />
+
+				<div className='card-body'>
+					<p>{productsData.name}</p>
+					<p>${productsData.price.toLocaleString()}</p>
+					{/* <button className='btn btn-dark' data-toggle="modal" data-target="#exampleModalCenter" onClick={handleSeeDetails}>
+						Xem chi tiết
+					</button> */}
+				</div>
+			</div>
+		);
+	}
+}
+
---
+++ shoeshop/src/modules/PhoneShop/ProductList.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+
+export default class Sanphamchitiet extends Component {
+  render() {
+    const {ProductDetails} = this.props;
+    const {id, name, alias, price, description, shortDescription, quantity,} = ProductDetails;
+    return (
+      <div>
+        						<h2>Thông Tin Chi Tiết</h2>
+
+        <table className='table'>
+							<tbody>
+                            <tr>
+								<td>Mã sản phẩm</td>
+								<td>{id}</td>
+							</tr>
+							<tr>
+								<td>Tên loại giày</td>
+								<td>{name}</td>
+							</tr>
+							<tr>
+								<td>Tên rút gọn</td>
+								<td>{alias}</td>
+							</tr>
+							<tr>
+								<td>Giá của sản phẩm</td>
+								<td>{price}</td>
+							</tr>
+							<tr>
+								<td>Mô tả chi tiết</td>
+								<td>{description}</td>
+							</tr>
+							<tr>
+								<td>Mô tả ngắn gọn</td>
+								<td>{shortDescription}</td>
+							</tr>
+                            <tr>
+								<td>Số lượng còn lại</td>
+								<td>{quantity}</td>
+                            </tr>
+                            </tbody>
+						</table>
+      </div>
+    )
+  }
+}
